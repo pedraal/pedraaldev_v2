726582c4b04455a747fec4e912136fbb93d85711
pierre golfier <pierre@codeur.com>
2020-07-05 11:09:35 +0200

    project init

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      Pedraal.dev
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      absolute
+      app
+    >
+      <span>PedraalDev &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
